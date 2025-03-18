9a6fba1ce646e820ab23f6324b8814e89f275127
Misofu <misofu@gmail.com>
2025-03-17 22:30:00 -0300

    Crear array y función agregar app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = []; // Creamos un array vacío para almacenar los nombres de los amigos
+
+function agregarAmigo(){ // Implementamos la función para agregar los nombres de los amigos
+    const inputAmigo = document.getElementById("amigo");
+    const nombreAmigo = inputAmigo.value.trim();
+
+    if (nombreAmigo === ""){
+        alert("Por favor, inserte un nombre válido");
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+
+    actualizarLista();
+
+    inputAmigo.value = "";
+    inputAmigo.focus();
+
+
+}
